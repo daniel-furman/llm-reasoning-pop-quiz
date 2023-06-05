fa252b392aa4ae8e8f5b237aa390af030945ba60
daniel-furman <dryanfurman@gmail.com>
2023-06-05 14:45:52 -0700

    support for falcon loras added

--- src/drf_llm_boilers/drf_llm_boilers.py
+++ src/drf_llm_boilers/drf_llm_boilers.py
@@ -3,6 +3,7 @@
 
 from typing import List
 import transformers
+import peft
 import torch
 import warnings
 
@@ -12,12 +13,11 @@ warnings.filterwarnings("ignore")
 # set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# from .utils import configure_openai
-
 
 class llm_boiler:
-    def __init__(self, model_id):
+    def __init__(self, model_id, lora):
         self.model_id = model_id
+        self.lora = lora
         for f_idx, run_function in enumerate(MODEL_FUNCTIONS):
             if run_function.__name__.lower() in self.model_id:
                 print(
@@ -30,12 +30,13 @@ class llm_boiler:
                     f"Run function recognized for {self.model_id}: {run_function.__name__.lower()}"
                 )
                 self.run_fn = run_function
-        self.model, self.tokenizer = self.load_fn(self.model_id)
+        self.model, self.tokenizer = self.load_fn(self.model_id, self.lora)
         self.name = self.run_fn.__name__.lower()
 
     def run(
         self,
         prompt,
+        lora,
         eos_token_ids,
         max_new_tokens,
         temperature,
@@ -44,11 +45,11 @@ class llm_boiler:
         top_k,
         num_return_sequences,
     ):
-        # try:
         return self.run_fn(
             self.model,
             self.tokenizer,
             prompt=prompt,
+            lora=lora,
             eos_token_ids=eos_token_ids,
             max_new_tokens=max_new_tokens,
             temperature=temperature,
@@ -57,8 +58,6 @@ class llm_boiler:
             top_k=top_k,
             num_return_sequences=num_return_sequences,
         )
-        # except:
-        # print("ERROR: something went wrong running model")
 
 
 # llm boilers code
@@ -73,30 +72,55 @@ MODEL_FUNCTIONS = []
 
 ## falcon models
 def falcon_loader(
-    model_id: str,
+    model_id: str,  # HF model id
+    lora: bool,  # whether or not model is lora fine-tuned with peft
 ):
-    # see source: https://huggingface.co/tiiuae/falcon-40b-instruct#how-to-get-started-with-the-model
+    if not lora:
+        # see source: https://huggingface.co/tiiuae/falcon-40b-instruct#how-to-get-started-with-the-model
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+        tokenizer.pad_token = tokenizer.eos_token
+    
+        bnb_config = transformers.BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+    
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_id,
+            quantization_config=bnb_config,
+            device_map={"": 0},
+            trust_remote_code=True,
+        )
+    
+        return model, tokenizer
 
-    # this will take a long time, to avoid: deploy the LLM as an API inference endpoint
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
-    tokenizer.pad_token = tokenizer.eos_token
+    if lora:
+        # see source: https://huggingface.co/dfurman/falcon-40b-chat-oasst1#first-load-the-model
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+        tokenizer.pad_token = tokenizer.eos_token
 
-    bnb_config = transformers.BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
+        config = peft.PeftConfig.from_pretrained(model_id)
+        
+        bnb_config = transformers.BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16
+        )
+        
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            config.base_model_name_or_path,
+            return_dict=True,
+            quantization_config=bnb_config,
+            device_map={"": 0},
+            trust_remote_code=True,
+        )
+        
+        model = peft.PeftModel.from_pretrained(model, model_id)
 
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_id,
-        quantization_config=bnb_config,
-        device_map={"": 0},
-        trust_remote_code=True,
-    )
-
-    return model, tokenizer
-
+        return model, tokenizer
 
 LOAD_MODEL_FUNCTIONS.append(falcon_loader)
 
@@ -105,6 +129,7 @@ def falcon(
     model: transformers.AutoModelForCausalLM,
     tokenizer: transformers.AutoTokenizer,
     prompt: str,
+    lora: bool,
     eos_token_ids: List[int],
     max_new_tokens: int = 128,
     do_sample: bool = True,
@@ -159,26 +184,48 @@ def falcon(
             bos_token_id=tokenizer.eos_token_id,
             streamer=streamer,
         )
-    if num_return_sequences == 1:
-        generated_text = tokenizer.decode(
-            output_tokens[0][len(inputs.input_ids[0]) :], skip_special_tokens=True
-        )
-        if generated_text[0] == " ":
-            generated_text = generated_text[1:]
-
-        return generated_text
-
-    else:
-        generated_text_list = []
-        for i in range(num_return_sequences):
+    if not lora:
+        if num_return_sequences == 1:
             generated_text = tokenizer.decode(
-                output_tokens[i][len(inputs.input_ids[0]) :], skip_special_tokens=True
+                output_tokens[0][len(inputs.input_ids[0]):], skip_special_tokens=True
             )
             if generated_text[0] == " ":
                 generated_text = generated_text[1:]
-            generated_text_list.append(generated_text)
+    
+            return generated_text
+    
+        else:
+            generated_text_list = []
+            for i in range(num_return_sequences):
+                generated_text = tokenizer.decode(
+                    output_tokens[i][len(inputs.input_ids[0]):], skip_special_tokens=True
+                )
+                if generated_text[0] == " ":
+                    generated_text = generated_text[1:]
+                generated_text_list.append(generated_text)
+    
+            return generated_text_list
 
-        return generated_text_list
+    if lora:
+        if num_return_sequences == 1:
+            generated_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+            generated_text = generated_text.split("<human>: ")[1].split("<bot>: ")[-1]
 
+            if generated_text[0] == " ":
+                generated_text = generated_text[1:]
+    
+            return generated_text
+    
+        else:
+            generated_text_list = []
+            for i in range(num_return_sequences):
+                generated_text = tokenizer.decode(output_tokens[i], skip_special_tokens=True)
+                generated_text = generated_text.split("<human>: ")[1].split("<bot>: ")[-1]
+    
+                if generated_text[0] == " ":
+                    generated_text = generated_text[1:]
+                generated_text_list.append(generated_text)
+    
+            return generated_text_list
 
 MODEL_FUNCTIONS.append(falcon)
